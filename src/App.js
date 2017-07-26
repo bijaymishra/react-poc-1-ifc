0b201ceed4b969dc9f506c1c0bd73bfe0f205ea8
bijaymishra <bbmishra86@Bijays-MacBook-Pro.local>
2017-07-26 10:11:01 +0530

    app.js commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Collapsible from 'react-collapsible';
-import Modal from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 import './Collapse.css';
 import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
-
 //import { ResponsiveReactGridLayout }       from 'react-grid-layout';
 import { WidthProvider }    from 'react-grid-layout';
 var PureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
@@ -83,7 +81,6 @@ var ResponsiveLocalStorageLayout = React.createClass({
         </ResponsiveReactGridLayout>
           </p>
       </div>
-      <App/>
       </div>
     );
   }
@@ -107,33 +104,6 @@ function saveToLS(key, value) {
     }));
   }
 }
-var App = React.createClass({
-    render: function () {
-        return <div>
-<button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  Launch demo modal
-</button>
-
-<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
-        <h4 className="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div className="modal-body">
-        ...
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
-    }
-});
 
 
 
